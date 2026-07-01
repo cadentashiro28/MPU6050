fcd0df3aee44064e239a956b9516328cfa26d37f
Caden Tashiro <cadentashiro@gmail.com>
2026-06-30 20:46:49 -0700

    Initial commit: MPU6050 driver bones utilizing I2C interrupts

---
+++ Core/Inc/mpu6050.h
@@ -0,0 +1,55 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: Jun 28, 2026
+ *      Author: caden
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+#include <stdint.h>
+#include "main.h"
+
+typedef struct {
+	int16_t accel_x, accel_y, accel_z;
+	int16_t gyro_x, gyro_y, gyro_z;
+} MPU6050_Data_t;
+
+// Initialization
+#define MPU6050_ADDR 0x68 << 1
+#define MPU6050_PWR_MGMT 0x6B
+#define MPU6050_CFG 0x1A
+#define MPU6050_SMPLRT_DIV 0x19
+#define MPU6050_GYRO_CFG 0x1B
+#define MPU6050_ACCEL_CFG 0x1C
+#define MPU6050_INT_ENABLE 0x38
+#define MPU6050_INT_CFG 0x37
+
+/**
+ * Data
+ * Note: Read is continuous starting from 0x3B, Notice gap between ACCEL and GYRO
+ * Temp sensings occupy these addresses
+ */
+#define MPU6050_ACCEL_X_H 0x3B
+#define MPU6050_ACCEL_X_L 0x3C
+#define MPU6050_ACCEL_Y_H 0x3D
+#define MPU6050_ACCEL_Y_L 0x3E
+#define MPU6050_ACCEL_Z_H 0x3F
+#define MPU6050_ACCEL_Z_L 0x40
+
+#define MPU6050_GYRO_X_H 0x43
+#define MPU6050_GYRO_X_L 0x44
+#define MPU6050_GYRO_Y_H 0x45
+#define MPU6050_GYRO_Y_L 0x46
+#define MPU6050_GYRO_Z_H 0x47
+#define MPU6050_GYRO_Z_L 0x48
+
+// Function Declarations
+HAL_StatusTypeDef MPU6050_Init(I2C_HandleTypeDef *hi2c);
+void MPU6050_Parse(uint8_t* raw_buf, MPU6050_Data_t *latest);
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+void MPU6050_ReadAll_IT(I2C_HandleTypeDef *hi2c, uint8_t *buf);
+
+#endif /* INC_MPU6050_H_ */
---
+++ Core/Src/mpu6050.c
@@ -0,0 +1,85 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: Jun 29, 2026
+ *      Author: caden
+ */
+
+#include "mpu6050.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+#define MPU6050_TIMEOUT 10
+
+static uint8_t raw_buf[14] = {0};
+static MPU6050_Data_t latest;
+
+/**
+ * Parses raw buffer into data struct
+ * Recall that idx 6 and 7 store temperature data
+ */
+void MPU6050_Parse(uint8_t* buf, MPU6050_Data_t *out) {
+	out->accel_x = (uint16_t) (buf[0] << 8  | buf[1]);
+	out->accel_y = (uint16_t) (buf[2] << 8  | buf[3]);
+	out->accel_z = (uint16_t) (buf[4] << 8  | buf[5]); // SKIP TEMP DATA
+	out->gyro_x =  (uint16_t) (buf[8] << 8  | buf[9]);
+	out->gyro_y =  (uint16_t) (buf[10] << 8 | buf[11]);
+	out->gyro_z =  (uint16_t) (buf[12] << 8 | buf[13]);
+}
+
+
+/**
+ * Activates upon I2C read transaction completion
+ * Triggers parsing of raw data into a data structure
+ */
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    if (hi2c->Instance == I2C1) {
+        MPU6050_Parse(raw_buf, &latest);
+    }
+}
+
+/**
+ * Activates upon INT pin
+ * Triggers MPU6050 I2C read transaction
+ */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_PIN == GPIO_PIN_8) {
+		MPU6050_ReadAll_IT(&hi2c1, raw_buf);
+	}
+}
+
+/**
+ * Initializes MPU6050 with preset configuration
+ * Returns HAL_OK
+ */
+HAL_StatusTypeDef MPU6050_Init(I2C_HandleTypeDef *hi2c) {
+	HAL_I2C_Init(hi2c);
+	uint8_t pwr = (1 << 0);
+	uint8_t cfg = (3 << 0);
+	uint8_t smplrt = (9 << 0); // 1000 / (1+9) = 100 Hz SMPRT
+	uint8_t gyro = (1 << 3);
+	uint8_t accel = (2 << 3);
+	uint8_t int_enable = (1 << 0);
+	uint8_t int_cfg = (1 << 4);
+
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_PWR_MGMT, I2C_MEMADD_SIZE_8BIT, &pwr, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_CFG, I2C_MEMADD_SIZE_8BIT, &cfg, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_SMPLRT_DIV, I2C_MEMADD_SIZE_8BIT, &smplrt, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_GYRO_CFG, I2C_MEMADD_SIZE_8BIT, &gyro, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_ACCEL_CFG, I2C_MEMADD_SIZE_8BIT, &accel, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_INT_ENABLE, I2C_MEMADD_SIZE_8BIT, &int_enable, 1, MPU6050_TIMEOUT);
+	HAL_I2C_Mem_Write(hi2c, MPU6050_ADDR, MPU6050_INT_CFG, I2C_MEMADD_SIZE_8BIT, &int_cfg, 1, MPU6050_TIMEOUT);
+	return HAL_OK;
+}
+
+/**
+ * Takes 14 readings starting from Accel_X_High up to Gyro_Z_Low
+ * Temperature occupies idx 6 and 7
+ *
+ * Parameters:
+ * hi2c - pointer to I2C handle
+ * buf - pointer to raw readings buffer
+ */
+void MPU6050_ReadAll_IT(I2C_HandleTypeDef *hi2c, uint8_t *buf) {
+	HAL_I2C_Mem_Read_IT(hi2c, MPU6050_ADDR, MPU6050_ACCEL_X_H, I2C_MEMADD_SIZE_8BIT, buf, 14);
+}
